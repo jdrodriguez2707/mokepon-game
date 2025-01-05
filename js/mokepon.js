3694e2391c3433d65f7d3717009695ddb080a3cc
jdrodriguez2707 <johandavid.jdr@gmail.com>
2025-01-05 14:27:32 -0500

    Add pet selection functionality
    Determinate which pet is selected by the user according to the input radio selected in HTML

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,8 +1,25 @@
 'use strict'
 
-function selectPlayerPet() {
-  alert("Player's pet selected")
+function startGame() {
+  const btnSelectPlayerPet = document.querySelector('#btn-select-pet')
+  btnSelectPlayerPet.addEventListener('click', selectPlayerPet)
 }
 
-const btnSelectPlayerPet = document.querySelector('#btn-select-pet')
-btnSelectPlayerPet.addEventListener('click', selectPlayerPet)
+function selectPlayerPet() {
+  // alert("Player's pet selected")
+  const hipodoge = document.querySelector('#hipodoge')
+  const capipepo = document.querySelector('#capipepo')
+  const ratigueya = document.querySelector('#ratigueya')
+
+  if (hipodoge.checked) {
+    alert("Player's pet selected: Hipodoge")
+  } else if (capipepo.checked) {
+    alert("Player's pet selected: Capipepo")
+  } else if (ratigueya.checked) {
+    alert("Player's pet selected: Ratigueya")
+  } else {
+    alert('Please select a pet')
+  }
+}
+
+startGame()
