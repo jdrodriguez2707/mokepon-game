b3a4a7ef74389a6b61d7078032728e1a185fac2d
jdrodriguez2707 <johandavid.jdr@gmail.com>
2025-01-06 14:57:55 -0500

    Add functionality to display selected pet name in the attack section

--- js/mokepon.js
+++ js/mokepon.js
@@ -8,17 +8,19 @@ function startGame() {
 function selectPlayerPet() {
   const petOptions = document.querySelectorAll('input[name="pet"]')
   let selectedPet = null
+  // span to display player's pet name
+  const playerPetSpan = document.querySelector('#player-pet-name')
+
   for (const pet of petOptions) {
     if (pet.checked) {
+      // get the text content of the label associated with the selected pet
       selectedPet = document.querySelector(`label[for="${pet.id}"]`).textContent
       break
     }
   }
-  alert(
-    selectedPet
-      ? `Player's pet selected: ${selectedPet}`
-      : 'Please select a pet'
-  )
+  selectedPet
+    ? (playerPetSpan.textContent = selectedPet)
+    : alert('Please select a pet')
 }
 
 startGame()
