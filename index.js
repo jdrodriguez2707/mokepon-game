a4af699eb5bed33533eabde8677f537c478106c1
jdrodriguez2707 <johandavid.jdr@gmail.com>
2025-02-21 15:42:01 -0500

    Add Express.js setup and update .gitignore for Node project

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello, Node!");
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express.js!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
